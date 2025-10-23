9fc81e57277048672b664d0959a30b280c8b570b
marco <marcokirsmanhirschvogel@gmail.com>
2025-10-23 02:36:13 -0300

    CreaciónCatálogo

---
+++ PW10/script.js
@@ -0,0 +1,19 @@
+// === catalogo de lujo, ingreso de autos ===
+const AUTOS = [
+  { id:"a1", marca:"BMW", modelo:"3.0 335i Coupe Sportive 306cv", anio:2013, km:11600, precio:41000000,
+    img:"./fotos/bm.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a2", marca:"DS", modelo:"7 crossback", anio:2021, km:24000, precio:46800000,
+    img:"./fotos/ds.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a3", marca:"Audi", modelo:"A7 Sportback", anio:2020, km:30000, precio:98000000,
+    img:"./fotos/audi.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a4", marca:"Porsche", modelo:"911", anio:1987, km:22000, precio:145000000,
+    img:"./fotos/porsche.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a5", marca:"Land Rover", modelo:"Range Rover Vogue", anio:2010, km:5560, precio:102000000,
+    img:"./fotos/rangerover.png", transmision:"AT", combustible:"Diésel" },
+  { id:"a6", marca:"Lexus", modelo:"LS 500", anio:2022, km:12000, precio:109000000,
+    img:"./fotos/lexus.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a7", marca:"Maserati", modelo:"Levante GranSport", anio:2020, km:28000, precio:99000000,
+    img:"./fotos/maserat.png", transmision:"AT", combustible:"Nafta" },
+  { id:"a8", marca:"Jaguar", modelo:"F-Type R", anio:2019, km:25000, precio:102000000,
+    img:"./fotos/jag.png", transmision:"AT", combustible:"Nafta" }
+];
